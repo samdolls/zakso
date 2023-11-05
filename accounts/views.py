d2ee10597f28ce58e91e50776fdb357e84c94819
maeng <20221153@sungshin.ac.kr>
2023-11-06 00:23:51 +0900

    fix:자동로그인,아이디저장

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import CustomUser
 from .forms import UserForm
 from django.contrib.auth import login, authenticate, logout
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.sessions.models import Session
+@csrf_protect
 
 #회원가입
 def signup(request):
@@ -18,23 +21,30 @@ def signup(request):
         form = UserForm()
     return render(request, 'signup.html', {'form':form})
 
-
-#로그인
+# 로그인
 def login_view(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
+        remember_me = request.POST.get('remember_me')
+        stay_login = request.POST.get('stay_login')
+
         user = authenticate(request, email=email, password=password)
         if user is not None:
             login(request, user)
+
+            if not remember_me:
+                request.session.set_expiry(0)  # 세션 유지 시간을 0으로(브라우저 끝나면 끝)
+            elif stay_login:
+                request.session.set_expiry(1209600)  # 세션 유지 시간을 2주로(자동 로그인한다면)
+
             return redirect('accounts:home')
-        else: 
-            return render(request, 'login.html', {'error':'이메일 또는 비밀번호가 올바르지 않습니다.'})
+        else:
+            return render(request, 'login.html', {'error': '이메일 또는 비밀번호가 올바르지 않습니다.'})
     else:
         return render(request, 'login.html')
 
-
-#로그아웃
+# 로그아웃
 def logout_view(request):
     logout(request)
     return redirect('accounts:login_view')
