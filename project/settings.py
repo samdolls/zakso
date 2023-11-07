6f8dd7e849d3c89f02ffea38c6bca5b0d6ca3d66
maeng <20221153@sungshin.ac.kr>
2023-11-08 00:09:02 +0900

    aws-allowed host update

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]
 
 
 # Application definition
