0b79c8c18347bcc79883a1db1e19fbbfd28b9436
maeng <20221153@sungshin.ac.kr>
2023-11-09 21:59:14 +0900

    도메인연결

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = [".ap-northeast-2.compute.amazonaws.com"]
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com",
+    ".zakso.shop",
+    ]
 
 
 # Application definition
