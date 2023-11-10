6ca39209a9a250da10d5aa55ec9c5a9f3c4e176f
samdolls <minjoon927@gmail.com>
2023-11-10 20:29:42 +0900

    feature/contactmaking: "게시물 관련 페이지 연결"

--- project/static/js/presentFundList.js
+++ project/static/js/presentFundList.js
@@ -1,14 +1,22 @@
 // progressbar 진행상황
 document.addEventListener("DOMContentLoaded", function() {
-  for(let i = 1; i<=4; i++) {
-    let price = parseInt(document.querySelector(`.totalMoney${i}`).textContent);
-    console.log(price);
-    let currentMoney = parseInt(document.querySelector(`.currentMoney${i}`).textContent);
-    let progress = (currentMoney / price) * 100;
-    let currentProgress = document.querySelector(`.bar${i}`);
-    currentProgress.style.width = progress + "%";
-    currentProgress.setAttribute("aria-valuenow", progress);
-    let currentPercent = document.querySelector(`.percent${i}`);
+  // 반복되는 요소들에 대한 NodeList를 가져옵니다.
+  let presentElements = document.querySelectorAll('.listBox');
+
+  // NodeList를 배열로 변환하여 반복합니다.
+  presentElements.forEach(function(presentElement, index) {
+    let totalMoney = parseInt(presentElement.querySelector('.totalMoney1').textContent);
+    let currentMoney = parseInt(presentElement.querySelector('.currentMoney1').textContent);
+    let progress = (currentMoney / totalMoney) * 100;
+    
+    // 각 요소에 대한 식별자를 사용하여 업데이트합니다.
+    let progressBar = presentElement.querySelector('.bar1');
+    progressBar.style.width = progress + '%';
+    progressBar.setAttribute('aria-valuenow', progress);
+
+    let currentPercent = presentElement.querySelector('.currentPercent');
     currentPercent.innerText = Math.round(progress);
-  }
+  });
 });
+
+
