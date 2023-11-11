40a33e9b3fb467e228851cd6fb541c48339b3205
maeng <20221153@sungshin.ac.kr>
2023-11-11 12:21:30 +0900

    fix:deatil수정

--- project/settings.py
+++ project/settings.py
@@ -89,9 +89,16 @@ WSGI_APPLICATION = "project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USERNAME"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": int(os.getenv("DATABASE_PORT")),
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+                    'charset':'utf8mb4',
+                   },
     }
 }
 
